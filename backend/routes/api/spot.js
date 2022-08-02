58e63680f629444817c2beb41ec55e0fdaa18dd3
edgar <edgarlee135@gmail.com>
2022-08-02 14:00:05 -0700

    still need token

--- backend/routes/api/spot.js
+++ backend/routes/api/spot.js
@@ -1,9 +1,16 @@
 const { requireAuth } = require("../../utils/auth");
 const express = require('express')
 const router = express.Router();
+const { Spot } = require('../../db/models')
 
-// router.get('/', requireAuth, async (req, res), {
+router.get('/', async (req, res) => {
+  const spot = await Spot.findAll()
+  res.json(spot)
+})
 
-// })
+router.get('/current', requireAuth, async (req, res) => {
+  const spot = await Spot.findAll()
+  res.json(spot)
+})
 
 module.exports = router;
