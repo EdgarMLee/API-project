25393339e163c25cc2bdd7489032c8134bd3b4db
edgar <edgarlee135@gmail.com>
2022-08-03 16:33:43 -0700

    fixed firstName

--- backend/routes/api/booking.js
+++ backend/routes/api/booking.js
@@ -1,9 +1,14 @@
-const { requireAuth } = require("../../utils/auth");
+const { requireAuth, restoreUser } = require("../../utils/auth");
 const express = require('express')
 const router = express.Router();
+const { Spot, Image, User, Review, Booking } = require('../../db/models')
+const { handleValidationErrors } = require('../../utils/validation');
+const { check } = require('express-validator');
+const { Op } = require("sequelize");
 
-// router.get('/', async (req, res), {
-
-// })
+//Get all of the Current User's Bookings
+router.get('/', requireAuth, restoreUser, async (req, res, next), {
+  
+})
 
 module.exports = router;
