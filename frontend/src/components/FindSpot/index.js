4bfda4c878887ab3d8acc4a493973b790c02cda7
edgar <edgarlee135@gmail.com>
2022-08-22 22:26:40 -0700

    styled details

--- frontend/src/components/FindSpot/index.js
+++ frontend/src/components/FindSpot/index.js
@@ -4,6 +4,7 @@ import { useHistory, useParams, Link } from 'react-router-dom';
 import {findSpot, allSpots} from '../../store/spots';
 import "./FindSpot.css";
 
+
 const FindSpot = () => {
   const {spotId} = useParams();
   const dispatch = useDispatch();
@@ -21,8 +22,11 @@ const FindSpot = () => {
       <div>
           <div className="nameSpot">{spot?.name}</div>
           {/* TODO IMPLEMENT A STAR ICON HERE */}
-           <div className='ratingSpot'>{spot?.avgRating}</div>
-           <div key={spot?.id} className='stateSpot'>{spot?.city}, {spot?.state}, {spot?.country}</div>
+           <div className='ratingSpot'>
+           <div className="fa-solid fa-star"/>
+            {spot?.avgRating} ·
+            </div>
+           <div key={spot?.id} className='stateSpot'> · {spot?.city}, {spot?.state}, {spot?.country}</div>
           <div className='imgDiv'>
            <img className='imageSpot' src={spot?.previewImage} alt="Image Not Available"/>
            </div>
